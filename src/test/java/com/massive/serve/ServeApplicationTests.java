ead4d89034cdf4fed6ad11444b3aa917c8ea0090
ilyesInzo <ilyes.mansour.3@gmail.com>
2021-08-25 11:34:28 +0200

    inti server

---
+++ src/test/java/com/massive/serve/ServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massive.serve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
